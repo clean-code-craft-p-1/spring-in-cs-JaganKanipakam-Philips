7fbcf9ed1d90833c3d69631b2065a676905ea1f3
Kanipakam Jagan <jagan.kanipakam@philips.com>
2026-06-22 10:48:04 +0530

    first commit

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats
+    {
+        public double average;
+        public double max;
+        public double min;
+    }
+
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers)
+        public Stats CalculateStatistics(List<double> numbers)
         {
-            //Implement statistics here
+            var stats = new Stats
+            {
+                average = double.NaN,
+                max = double.NaN,
+                min = double.NaN
+            };
+
+            if (numbers == null || numbers.Count == 0)
+            {
+                return stats;
+            }
+
+            double sum = 0;
+            double max = double.NegativeInfinity;
+            double min = double.PositiveInfinity;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+                if (number > max) max = number;
+                if (number < min) min = number;
+            }
+
+            stats.average = sum / numbers.Count;
+            stats.max = max;
+            stats.min = min;
+
+            return stats;
         }
     }
 }
